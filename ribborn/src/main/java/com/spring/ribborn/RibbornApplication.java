9160c5e3aea245852cbc763f47f9c2738e200348
ParkSungGyu1 <steven0507@naver.com>
2022-06-24 16:13:11 +0900

    기초 환경설정

---
+++ ribborn/src/main/java/com/spring/ribborn/RibbornApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ribborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbornApplication.class, args);
+	}
+
+}
---
+++ ribborn/src/test/java/com/spring/ribborn/RibbornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ribborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
