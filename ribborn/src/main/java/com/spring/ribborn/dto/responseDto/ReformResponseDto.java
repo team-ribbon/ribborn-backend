24d8b1caada5df47a72836234f04fd94cf668554
songryel-park <fuf3413@gmail.com>
2022-07-06 03:23:27 +0900

    리폼 조회

---
+++ ribborn/src/main/java/com/spring/ribborn/dto/responseDto/ReformResponseDto.java
@@ -0,0 +1,37 @@
+package com.spring.ribborn.dto.responseDto;
+
+import com.spring.ribborn.model.Content;
+import com.spring.ribborn.model.Images;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class ReformResponseDto {
+    @Builder
+    public static class ReformMain {
+        private Long id;
+        private Images image;
+        private String nickname;
+        private String title;
+        private String category;
+        private String region;
+    }
+
+    @Builder
+    public static class ReformDetail {
+        private Long id;
+        private String nickname;
+        private List<Images> images;
+        private String title;
+        private String category;
+        private List<Content> content;
+        private String region;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+    }
+}
---
+++ ribborn/src/main/java/com/spring/ribborn/repository/ImagesRepository.java
@@ -0,0 +1,8 @@
+package com.spring.ribborn.repository;
+
+import com.spring.ribborn.model.Images;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImagesRepository extends JpaRepository<Images, Long> {
+    Images findTopByPostIdOrderByCreatedAtDesc(Long postId);
+}
