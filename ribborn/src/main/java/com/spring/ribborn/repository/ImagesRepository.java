3d732d8618dea61fbdd0bd688ea4b3df1a428338
songryel-park <fuf3413@gmail.com>
2022-07-06 14:54:22 +0900

    .

--- ribborn/src/main/java/com/spring/ribborn/repository/ImagesRepository.java
+++ ribborn/src/main/java/com/spring/ribborn/repository/ImagesRepository.java
@@ -3,6 +3,8 @@ package com.spring.ribborn.repository;
 import com.spring.ribborn.model.Images;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImagesRepository extends JpaRepository<Images, Long> {
+import java.util.List;
+
+public interface ImagesRepository extends JpaRepository<Images, String> {
     Images findTop1ByPostIdOrderByCreateAtDesc(Long postId);
 }
