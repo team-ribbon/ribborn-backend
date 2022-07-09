42bcef6114712f94e160e3c2038305b78669ceee
ParkSungGyu1 <steven0507@naver.com>
2022-07-09 19:36:31 +0900

    좋아요 기능 추가

---
+++ ribborn/src/main/java/com/spring/ribborn/dto/requestDto/LoveRequestDto.java
@@ -0,0 +1,10 @@
+package com.spring.ribborn.dto.requestDto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LoveRequestDto {
+    private boolean love;
+}
