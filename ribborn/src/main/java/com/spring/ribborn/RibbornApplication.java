61f058c30d684b47ce28dbc5ed1edd7feb2e4233
lky8967 <lky8967@gmail.com>
2022-07-22 02:52:10 +0900

    병합전 커밋

--- ribborn/src/main/java/com/spring/ribborn/RibbornApplication.java
+++ ribborn/src/main/java/com/spring/ribborn/RibbornApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//import javax.annotation.PostConstruct;
+//import java.util.TimeZone;
+
 @EnableJpaAuditing
 @EnableJpaRepositories
 @SpringBootApplication
